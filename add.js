761b45e84621334345cca6440aa50aa1d1b27b81
NANDHINI19ITR051 <82797895+NANDHINI19ITR051@users.noreply.github.com>
2021-09-06 13:01:26 +0530

    Add files via upload

---
+++ add.js
@@ -0,0 +1,6 @@
+h=parseInt(prompt("Enter a number"))
+g=parseInt(prompt("Enter another number"))
+document.writeln("<h3>Addition of two numbers:</h3>"+h+" +");
+document.writeln(g+" =");
+var e=g+h;
+document.writeln(e);
